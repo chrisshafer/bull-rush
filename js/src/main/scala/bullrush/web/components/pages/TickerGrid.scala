284b96331aa304dd7b2f51a3d03ebee6f8784e6b
Chris Shafer <chrismail1492@gmail.com>
2015-09-06 21:29:36 -0500

    Added additional details, changed indicators

--- js/src/main/scala/bullrush/web/components/pages/TickerGrid.scala
+++ js/src/main/scala/bullrush/web/components/pages/TickerGrid.scala
@@ -3,10 +3,9 @@ package bullrush.web.components.pages
 import bullrush.model.TickerDetails
 import bullrush.web.stores.TickerStore
 import japgolly.scalajs.react.vdom.prefix_<^._
-import japgolly.scalajs.react.{BackendScope, ReactComponentB}
+import japgolly.scalajs.react.{ReactNode, BackendScope, ReactComponentB}
 
 object TickerGrid {
-
   case class State(tickers: Seq[TickerDetails])
   class Backend($: BackendScope[Unit, State]){
 
@@ -21,7 +20,20 @@ object TickerGrid {
     }
 
   }
+  val curFmt = "%.2f"
 
+
+  def details(ticker: TickerDetails): ReactNode = {
+
+    <.div( ^.className := "details",
+      detail("50 Day Avg", ticker.stats.fiftyDayMovingAverage.get.toString),
+      detail("52 week", ticker.HighLow.yearLow.get.formatted(curFmt) +" - "+ticker.HighLow.yearHigh.get.formatted(curFmt)),
+      detail("Mkt Cap", ticker.stats.marketCap.get),
+      detail("P/E", if(ticker.ratios.peRatio.isDefined) ticker.ratios.peRatio.get.formatted(curFmt)  else " - " ),
+      detail("EPS", ticker.ratios.earningsPerShare.get.formatted(curFmt)),
+      detail("Range", ticker.HighLow.dayLow.get.formatted(curFmt)  + " - " + ticker.HighLow.dayHigh.get.formatted(curFmt))
+    )
+  }
   def detail (key: String, value: String) = {<.div( ^.className := "row",
     <.span( ^.className := "detail",
       <.span( ^.className := "detail-key", key ),
@@ -29,27 +41,24 @@ object TickerGrid {
     )
   }
   def priceComponent (price: Double, change: Double, percentChange: String) ={
+    val color : String = if(change < 0) "negative" else if(change > 0) "positive" else ""
     <.div( ^.className := "price-display",
-      <.span(^.className := "up-indicator"),
-      <.span( ^.className := "price row", price),
-      <.span( ^.className := "change row", change),
-      <.span( ^.className := "change row", percentChange),
-      <.span(^.className := "down-indicator")
+      <.span( ^.className := "change row "+color, change.formatted(curFmt)),
+      <.span( ^.className := "price row ", price.formatted(curFmt)),
+      <.span( ^.className := "change row "+color, percentChange)
     )
   }
 
+  def indicatorColor(change: Double) = if(change < 0) "negative" else if(change > 0) "positive" else "no-change"
   def tickerCard(ticker: TickerDetails) ={
     <.li(
-      <.div(^.className := "ticker-card",
-        <.div( ^.className := "small-8 medium-6 columns",
+      <.div(^.className := "ticker-card "+ indicatorColor(ticker.stats.change.get),
+        <.div( ^.className := "small-8 medium-7 large-6 columns",
           <.span( ^.className := "row ticker", ticker.stats.ticker.get),
           <.span( ^.className := "row company-name", ticker.stats.name.get.replace("Common Stock","")),
-          <.div( ^.className := "details",
-            detail("50 Day Avg", ticker.stats.fiftyDayMovingAverage.get.toString),
-            detail("52 week", ticker.HighLow.yearLow.get.toString +" - "+ticker.HighLow.yearHigh.get.toString)
-          )
+          details(ticker)
         ),
-        <.div( ^.className := "small-4 medium-6 columns",
+        <.div( ^.className := "small-4 medium-5 large-6 columns",
           priceComponent(ticker.stats.lastPrice.get,ticker.stats.change.get,ticker.stats.percentChange.get)
         )
       )
